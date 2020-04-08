b1f6d2f9cdfcc9adaf9808027d6c945c0048f9fe
dzil123 <5725958+dzil123@users.noreply.github.com>
2020-04-08 02:59:56 -0700

    yay

---
+++ js/lib.js
@@ -0,0 +1,3 @@
+(function (global) {
+    Vue.use(BaklavaJSRendererVue.BaklavaVuePlugin);
+})(this);
--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 (function (global) {
-    console.log("Hello");
+    global.editor = new BaklavaJS.Editor();
+    global.plugin = new BaklavaJSRendererVue.ViewPlugin();
+    editor.use(plugin);
+
+    global.app = new Vue({
+        el: "#app",
+        data: {
+            viewPlugin: plugin,
+        },
+    });
 })(this);
